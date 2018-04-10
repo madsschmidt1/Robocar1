a431e6fb27f625520bea0431c753769d26cd128a
madsschmidt1 <37693676+madsschmidt1@users.noreply.github.com>
2018-04-10 09:23:52 +0200

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,11 @@
+import time
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(3,GPIO.OUT)
+GPIO.setup(4,GPIO.OUT)
+
+#
+
+GPIO.output(3,GPIO.HIGH)
+GPIO.output(4,GPIO.LOW)
